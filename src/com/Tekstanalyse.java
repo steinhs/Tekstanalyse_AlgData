fd0d152a3b3edb92b4f339b23697a92fd79d70af
Stein Hanssen Sæstad <saestads@gmail.com>
2021-03-24 17:41:12 +0100

    Final commit. Works as intended.

--- src/com/Tekstanalyse.java
+++ src/com/Tekstanalyse.java
@@ -6,50 +6,15 @@ import org.w3c.dom.Node;
 import java.io.*;
 import java.util.*;
 /**
- * Stein Hanssen Sæstad -
- * Frist Fredag 26.Mars 23:59
- * https://hiof.instructure.com/courses/4861/assignments/17947
-
- * Forskjellige forfattere bruker ofte de samme ordene, men med varierende hyppighet.
- * Denne oppgaven går ut på å lese en tekst og registrere alle ordene som forekommer i teksten
- * i et binært søketre.
-
- * Skriv et program som leser tekst fra en fil (eller fra standard input) og deler teksten opp i
- * forskjellige ord (Lenker til en ekstern side.). Et ord regnes å bestå bare av bokstaver, og et
- * ord er slutt når det kommer et tegn som ikke er en bokstav. Gjør om alle bokstavene i hvert ord
- * til en stor bokstav, slik at det ikke blir forskjell på ord som kommer i begynnelsen av en
- * setning og ord som kommer inne i en setning.
-
- * Hvert ord som leses fra teksten skal legges inn i et binært søketre som initiellt er tomt.
- * Hvis ordet allerede finnes fra før, skal det ikke settes inn på nytt, men en teller i noden
- * som registrerer antall forekomster av ordet skal oppdateres.
-
- * Etter at hele teksten er lest skal alle ordene som forekom skrives ut i alfabetisk rekkefølge,
- * sammen med antallet ganger ordet forekom. Hvis treet er riktig bygd opp er det lett å skrive ut
- * ordene i alfabetisk rekkefølge ved å traversere treet på en bestemt måte.
-
- * Skriv hele programmet fra bunnen av, inkludert koden for innsetting av noder i søktreet.
- */
-/**
- * Lese tekst fra fil (txt)
- * Dele teksten opp i forskjellige ord, et ord regnes å bestå bare av bokstaver, og et
- * ord er slutt når det kommer et tegn som ikke er en bokstav (komma, space, punktum).
- * Gjør om alle bokstaver i hvert ord til en stor bokstav, slik at det ikke blir forskjell på ord
- * med store og små bokstaver.
+ * Stein Hanssen Sæstad
  *
- * Hvert ord som leses skal legges til i binært søketre som initiellt er tomt.
- * Finnes ordet fra før av skal det ikke legges til på nytt, men legges til teller i noden som registrerer
- * antall forekomster.
- *
- * Når alle ord er lest opp, skal alle ord skrives ut i alfabetisk rekkefølge og med antall ordet forekom.
- * Feks: ALBERT(1), BETOVEN(4), HEI(2), STEIN(2), . . . .
- *
- * Utskriving skal være lett å skrive ut i rekkefølgen ved å traversere treet på en bestemt måte.
+ * Kode er basert på JH-AlgDat - binarySearchTree.java og tilpasset oppgaven.
+ * Inkludert i prosjektet er lorem20, 300 og 3000 .txt filer.
  */
 
 public class Tekstanalyse {
+    //Counters for words
     int totalWordsRead = 0, uniqueWordsRead = 0;
-
 
     public static void main(String[] args) throws IOException {
         Tekstanalyse tekstanalyse = new Tekstanalyse();
@@ -58,25 +23,20 @@ public class Tekstanalyse {
         //Write data
         tekstanalyse.inorder();
         tekstanalyse.wordData();
-    }
 
-    public Tekstanalyse() throws IOException  {
     }
 
     /** INNER CLASS FOR BINARY TREE NODES WITH STRING DATA */
     public static class TreeNode{
         String data;
         int timesOccurred;
-        char[] dataChar;
         TreeNode left, right;
 
         public TreeNode(String value){
             this.timesOccurred = 1;
             data = value;
-            dataChar = data.toCharArray();
             left = right = null;
         }
-
         public void addTimesOccurred(){
             this.timesOccurred = this.timesOccurred+1;
         }
@@ -85,102 +45,87 @@ public class Tekstanalyse {
         }
     }
 
-    /** ITERATIVE SEARCH IN BINARY TREE WITH STRINGS FIRST CHARACTER
-     * Edited code from JH-AlgDat binarySearchTree.java
-     * Will search only based off first char of word */
-    public boolean search(String word){
-        TreeNode current = root;
-
-        char[] wordChar = word.toCharArray();
-
-        while (current != null){
-            if (current.data == word)
-                return true;
-            if (wordChar[0] < current.dataChar[0])
-                current = current.left;
-            else
-                current = current.right;
-        }
-        return false;
-    }
-
     /** READS FILE INPUT AND RUNS insert() METHOD */
     public void insertFromFile() throws FileNotFoundException {
 
-        /** READS INPUT FROM USER */
-        System.out.println("Enter name of text-file: \n(Existing files in solution are:\n- lorem20.txt\n- lorem300.txt\n- lorem600.txt\n- lorem3000.txt ");
-        //Scanner sc = new Scanner(System.in); File file = new File(sc.next());
-        /** READS LOREM FILE EXISTING IN SOLUTION PROJECT */
-        File file = new File("lorem20.txt");
-        Scanner in = new Scanner(file);
+        // READS INPUT FROM USER
+        System.out.println("Existing files in solution are:\n- lorem20.txt\n- lorem300.txt\n- lorem3000.txt\n\nEnter name of text-file:");
+        Scanner sc = new Scanner(System.in);
+        File file = new File(sc.next());
 
-        String str;
+        // Pre-defined paths
+        //File file = new File("lorem20.txt");
+        //File file = new File("lorem300.txt");
+        //File file = new File("lorem3000.txt");
+
+        Scanner in = new Scanner(file);
         // Runs through words, removes excess characters and makes all uppercase. Then runs insert()
         while (in.hasNext()){
             String word = in.next();
-            str = word.toUpperCase();
-            str = str.replaceAll("[^a-zA-Z0-9]", "");
-            if (str==""){ }
+            word = word.toUpperCase();
+            word = word.replaceAll("[^a-zA-Z0-9]", "");
+            // If empty string, skip, else insert
+            if (word==""){}
             else {
-                insert(str);
+                insert(word);
             }
         }
     }
+
     /** INSERT NEW WORD INTO BINARY SEARCH TREE */
     public void insert(String value){
         totalWordsRead++; //counter for total words
         TreeNode newNode = new TreeNode(value);
-        char[] valueChar = value.toCharArray();
 
         if (isEmpty()){
             root = newNode;
             uniqueWordsRead++;
             return;
         }
-
         TreeNode current = root;
         boolean finished = false;
         while (!finished){
+            //Checks if current word is the same as word getting inserted. If true > +1 to existing word
             if (value.equals(current.data)) {
                 current.addTimesOccurred();
                 finished = true;
             }
             else {
-                    if (valueChar[0] < current.dataChar[0]) {
-                        if (current.left == null) {
-                            current.left = newNode;
-                            uniqueWordsRead++;
-                            finished = true;
-                        } else current = current.left;
+                // Compares strings. compareTo returns int with alphabetical difference. If true > go to left side
+                if (value.compareTo(current.data) < 0) {
+                    // If empty on left side, insert and exit while-loop
+                    if (current.left == null) {
+                        current.left = newNode;
+                        uniqueWordsRead++;
+                        finished = true;
                     }
-
-                    else {
-                        if (current.right == null){
-                            current.right = newNode;
-                            uniqueWordsRead++;
-                            finished = true;
-                        }
-                        else current = current.right;
+                    // Else not empty, go into left side and repeat while-loop
+                    else current = current.left;
+                }
+                // Else go to right side
+                else {
+                    // If empty on right side, insert and exit while-loop
+                    if (current.right == null){
+                        current.right = newNode;
+                        uniqueWordsRead++;
+                        finished = true;
                     }
+                    // Else not empty, go into right side and repeat while-loop
+                    else current = current.right;
+                }
             }
         }
     }
 
-
     /** ROOT OF ENTIRE SEARCH TREE */
     public TreeNode root;
-
-    /** CONSTRUCTOR, CREATES EMPTY TREE */
-    public void binarySearchTree(){
-        root = null;
-    }
 
     /** CHECKS FOR EMPTY TREE */
     public boolean isEmpty(){
         return (root == null);
     }
 
-    /** PRINT TREE */
+    /** PRINT TREE WORDS AND OTHER DATA */
     public void inorder(){
         System.out.println("Inorder: ");
         inorder(root);
@@ -197,6 +142,5 @@ public class Tekstanalyse {
     public void wordData(){
         System.out.println("Total words read: " + totalWordsRead + "\nUnique words read: "+ uniqueWordsRead);
     }
-
 }
 
